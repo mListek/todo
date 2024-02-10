a38748f822c38731468ba328dc7f8436ed13812d
mListek <michal.listex@gmail.com>
2024-02-10 21:13:28 +0100

    created model and dummy data

---
+++ data/dummy-data.js
@@ -0,0 +1,10 @@
+import List from "../model/list";
+
+export const LISTS = [
+  new List(0, "Default List", [
+    "do the first thing",
+    "make dinner",
+    "create an application",
+  ]),
+  new List(1, "Groceries", ["bananas", "bread", "pizza"]),
+];
---
+++ model/list.js
@@ -0,0 +1,9 @@
+class List {
+  constructor(id, name, todos) {
+    this.id = id;
+    this.name = name;
+    this.todos = todos;
+  }
+}
+
+export default List;
