c9fdfec3d11a3bceaa95e9e0de6b3f3b74322926
mListek <michal.listex@gmail.com>
2024-02-12 19:19:25 +0100

    added todo model

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,16 +1,27 @@
 import List from "../model/list";
+import Todo from "../model/todo";
 
 export const LISTS = [
   new List(0, "Default List", [
-    "do the first thing",
-    "make dinner",
-    "create an application",
+    new Todo(0, "do the first thing"),
+    new Todo(1, "make dinner"),
+    new Todo(2, "create an application"),
   ]),
-  new List(1, "Groceries", ["bananas", "bread", "pizza"]),
-  new List(2, "Test List XX", ["test item"]),
+  new List(1, "Groceries", [
+    new Todo(3, "bananas"),
+    new Todo(4, "bread"),
+    new Todo(5, "pizza"),
+  ]),
+  new List(2, "Test List XX", [new Todo(6, "test todo")]),
   new List(3, "Test List XX", []),
   new List(4, "Test List XX", []),
   new List(5, "Test List XX", []),
   new List(6, "Test List XX", []),
   new List(7, "Test List XX", []),
+  new List(8, "Test List XX", []),
+  new List(9, "Test List XX", []),
+  new List(10, "Test List XX", []),
+  new List(11, "Test List XX", []),
+  new List(12, "Test List XX", []),
+  new List(13, "Test List XX", []),
 ];
